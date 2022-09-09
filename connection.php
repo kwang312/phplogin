42c194947b29356fb725b7cfe4953d07e1cf3ef9
kwang312 <113079061+kwang312@users.noreply.github.com>
2022-09-09 13:44:30 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbhost = "mradb.cjdclftyxl60.ap-southeast-1.rds.amazonaws.com";
+$dbhost = "mra-db.cjdclftyxl60.ap-southeast-1.rds.amazonaws.com";
 $dbuser = "admin";
 $dbpass = "quang123";
 $dbname = "login_sample_db";
